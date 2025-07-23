7b940abd4f82a73fd72ccf0abe9c02f6355a7906
TomDeRybel <tom.derybel@robocow.be>
2025-07-23 12:56:24 +0200

    Modified ST7796 driver, cfr the ST7789 driver.

--- src/models/st7796.rs
+++ src/models/st7796.rs
@@ -9,6 +9,8 @@ use crate::{
     ConfigurationError,
 };
 
+use super::InitEngine;
+
 /// ST7796 display in Rgb565 color mode.
 pub struct ST7796;
 
@@ -16,25 +18,23 @@ impl Model for ST7796 {
     type ColorFormat = Rgb565;
     const FRAMEBUFFER_SIZE: (u16, u16) = (320, 480);
 
-    fn init<DELAY, DI>(
+    fn init<IE>(
         &mut self,
-        di: &mut DI,
-        delay: &mut DELAY,
         options: &ModelOptions,
-    ) -> Result<SetAddressMode, ModelInitError<DI::Error>>
+        ie: &mut IE,
+    ) -> Result<SetAddressMode, ModelInitError<IE::Error>>
     where
-        DELAY: DelayNs,
-        DI: Interface,
+        IE: InitEngine,
     {
-        if !matches!(
-            DI::KIND,
-            InterfaceKind::Serial4Line | InterfaceKind::Parallel8Bit | InterfaceKind::Parallel16Bit
-        ) {
-            return Err(ModelInitError::InvalidConfiguration(
-                ConfigurationError::UnsupportedInterface,
-            ));
-        }
+        // if !matches!(
+        //     DI::KIND,
+        //     InterfaceKind::Serial4Line | InterfaceKind::Parallel8Bit | InterfaceKind::Parallel16Bit
+        // ) {
+        //     return Err(ModelInitError::InvalidConfiguration(
+        //         ConfigurationError::UnsupportedInterface,
+        //     ));
+        // }
 
-        super::ST7789.init(di, delay, options)
+        super::ST7789.init(options, ie)
     }
 }
